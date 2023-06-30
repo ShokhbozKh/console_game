69cf02285e14375899af896692443b48d447b918
Shohboz <shohboztuit@gmail.com>
2023-06-30 18:28:42 +0500

    Add project files.

---
+++ game/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int card = rnd.Next(100);     // creates a number between 0 and 99
+
+            int t=0;
+            while (true)
+            {
+                t=t+1;
+
+                Console.Write("Son kiriting: ");
+                int n=Convert.ToInt32(Console.ReadLine());
+                
+                if(n==card)
+                {
+                    Console.WriteLine($"Tabriklaymiz siz {(t)} Urinishda o\'ylangan sonni topdingiz!! ");
+                        break;
+                }
+                else if (n > card)
+                {
+                    Console.WriteLine("Kichikroq son kiriting !!");
+                }
+                else if (n < card)
+                {
+                    Console.WriteLine("Kattaroq son kiriting !!");
+                }
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
